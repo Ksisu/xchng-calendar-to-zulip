1641b77f15d320f4755072052486068cfc2698e7
Kristóf Horváth <horkris@gmail.com>
2020-03-03 10:29:42 +0100

    Optionally disable ssl check

--- xchng_calendar.py
+++ xchng_calendar.py
@@ -1,10 +1,16 @@
 import os, datetime
 from exchangelib import Credentials, Account, EWSDateTime, EWSTimeZone, UTC
+from exchangelib.protocol import BaseProtocol, NoVerifyHTTPAdapter
 
 user = os.environ['XCHNG_USER']
 password = os.environ['XCHNG_PASS']
 calendar = os.environ['XCHNG_CALENDAR']
 timezone = os.getenv('XCHNG_TIMEZONE')
+
+ssl_check = os.getenv('XCHNG_DISABLE_SSL_CHECK')
+
+if ssl_check is not None:
+    BaseProtocol.HTTP_ADAPTER_CLS = NoVerifyHTTPAdapter
 
 if timezone is None:
     timezone = UTC
